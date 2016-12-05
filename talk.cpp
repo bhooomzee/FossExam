ef6a766c81cbbc1c1281ebdfddb0b0a05de57407
bhooomzee <bhoomib04@gmail.com>
2016-12-05 11:52:30 +0530

    Add files via upload

---
+++ talk.cpp
@@ -0,0 +1,14 @@
+// MY FIRST C PROGRAM
+#include <stdio.h>
+int main()
+{
+	int a = 2;
+	double const b = 4;
+	float c = 1.5;
+	char d = 'A';
+	printf("the value of a is %d\n",a);
+	printf("the value of b is %lf\n",b);
+	printf("the value of c is %2f\n",c);
+	printf("the value of d is %c\n",d);
+	return 0;
+}
